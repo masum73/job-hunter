e334880a8a896381a9d5bb9981c4261aa60e4dd1
Masum Rahman <masumrahman73@gmail.com>
2023-04-12 00:46:04 +0600

    header and homeBanner button updated

--- src/components/Banner/HomeBanner.jsx
+++ src/components/Banner/HomeBanner.jsx
@@ -6,6 +6,7 @@ const HomeBanner = () => {
             <div className='max-w-xl'>
                 <h3 className='text-7xl font-bold'>One Step <br /> Closer To Your <br /> <span className='text-indigo-400'>Dream Job</span></h3>
                 <p className='my-4'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                <button className='btn-primary'>Get Started</button>
             </div>
             <div>
                 <img src="/src/assets/All Images/man.png" alt="" />
