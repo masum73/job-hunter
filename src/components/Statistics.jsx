2594141f9df5dccaf5662929939f7587bde04cc8
Masum Rahman <masumrahman73@gmail.com>
2023-04-12 05:01:16 +0600

    rechart installed and applied on statistics component

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { CartesianGrid, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const marksArray = [
+        {
+            assignmentNumber: 'Assignment 1',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 2',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 3',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 4',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 5',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 6',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 7',
+            assignmentMarks: 60,
+        },
+        {
+            assignmentNumber: 'Assignment 8',
+            assignmentMarks: 60,
+        }
+    ]
     return (
-        <div>
-            <h3>Statistics</h3>
+        <div className='my-12 mx-auto px-12 flex flex-col justify-center items-center'>
+            <h2 className='text-3xl font-bold text-center my-5'>Assignments Marks Scatter Chart</h2>
+            <ScatterChart
+                width={1000}
+                height={500}
+                data={marksArray}
+                className='my-5'
+            >
+                <CartesianGrid></CartesianGrid>
+                <Scatter data={marksArray} fill="#8884d8"></Scatter>
+                {/* <Scatter dataKey="assignmentNumber"></Scatter>
+                <Scatter dataKey="assignmentMarks"></Scatter> */}
+                <XAxis dataKey="assignmentNumber"/>
+                <YAxis dataKey="assignmentMarks" unit=' marks'/>
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                
+            </ScatterChart>
         </div>
     );
 };
