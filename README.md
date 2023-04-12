1a32c3c207e29511cd2f49afe0ee0eee232712cf
Masum Rahman <masumrahman73@gmail.com>
2023-04-12 22:16:55 +0600

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-<!-- JOB HUNTER -->
+# Job Hunter Starter Repository
+
+ Best job hunting platform for applying jobs for different companies.
+
+## Live Link
+Hosted in Netlify -> [Job Hunter](https://herogadget.netlify.app/)
+
+## Best Features
+Apply for jobs 
+Reading blogs
--- src/components/SingleAppliedJob.jsx
+++ src/components/SingleAppliedJob.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 const SingleAppliedJob = ({ aJob }) => {
     const { id, companyLogo, jobTitle, companyName, type, time, location, salary } = aJob
     return (
-        <div className='flex  justify-between items-center mx-auto border border-gray-500 w-full p-5 my-3 border-gray-300'>
+        <div className='flex  justify-between items-center mx-auto border rounded-md w-full p-5 my-3 border-gray-300'>
             <div className='flex w-11/12'>
-                <div className='w-48 h-48 flex justify-center items-center bg-slate-100'>
+                <div className='w-48 h-48 flex justify-center items-center bg-slate-100 rounded-md'>
                     <img className='' src={companyLogo} alt="" />
                 </div>
                 <div className='px-12'>
                     <h2 className='text-xl font-semibold'>{jobTitle}</h2>
                     <h2 className='my-2'>{companyName}</h2>
                     <div className='flex justify-start items-center py-3'>
-                        <h2 className='border text-indigo-500 border-indigo-500 mr-5 p-2'>{type}</h2>
-                        <h2 className='border text-indigo-500 border-indigo-500 p-2'>{time}</h2>
+                        <h2 className='border rounded-md text-indigo-500 border-indigo-500 mr-5 p-2'>{type}</h2>
+                        <h2 className='border rounded-md text-indigo-500 border-indigo-500 p-2'>{time}</h2>
                     </div>
                     <div className='flex justify-center items-center gap-2'>
                         <img src="/src/assets/Icons/Location Icon.png" alt="" />
