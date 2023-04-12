c4333ae2d57438192364270b03ab809edb181c3a
Masum Rahman <masumrahman73@gmail.com>
2023-04-12 15:44:06 +0600

    singleJobDetails design and data displayed on UI

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -2,21 +2,22 @@ import React, { useContext } from 'react';
 import { JobContext } from '../App';
 import CommonBanner from './Banner/CommonBanner';
 import SingleJobDetails from './SingleJobDetails';
+import { useParams } from 'react-router-dom';
 
 const JobDetails = () => {
     const jobs = useContext(JobContext)
+    const params = useParams()
+    
+    const presentJob = jobs.find(pJob => parseInt(params.id) === pJob.id)
+
     return (
         <>
             <CommonBanner title='Job Details'></CommonBanner>
             <div className='w-9/12 mx-auto'>
                 <div className='py-96'>
-                    <h2>Job Details: {jobs.length}</h2>
-                    {
-                        jobs.map(job => <SingleJobDetails
-                        key={job.id}
-                        job={job}
-                        ></SingleJobDetails>)
-                    }
+                    <SingleJobDetails 
+                    presentJob = {presentJob}
+                    ></SingleJobDetails>
                 </div>
             </div>
         </>
--- src/components/SingleJobDetails.jsx
+++ src/components/SingleJobDetails.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
-const SingleJobDetails = ({job}) => {
-    const {type} = job
+const SingleJobDetails = ({ presentJob }) => {
+    const { jd, experiences, salary, jobTitle, email } = presentJob
     return (
-        <div>
-            <h2>{type}</h2>
+        <div className='flex justify-center items-start '>
+            <div className='w-3/4'>
+                <h2> <span className='font-bold text-xl'>Job Description:</span> {jd}</h2>
+                <h2><span className='font-bold text-xl'>Experience:</span> {experiences}</h2>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+            <div className='bg-indigo-50 w-full p-3 rounded-md'>
+                <h2 className='font-bold text-xl'>Job Details</h2>
+                <h1><span className='font-semibold text-md'>Salary:</span> {salary}</h1>
+                <h1><span className='font-semibold text-md'>Job Title:</span> {jobTitle}</h1>
+                <h2 className='font-bold text-xl'>Contact Information</h2>
+                <h1><span className='font-semibold text-md'>Email:</span> {email}</h1>
+            </div>
+            <div>
+                <button className='btn-apply mt-5'>Apply Now</button>
+            </div>
+            </div>
         </div>
     );
 };
